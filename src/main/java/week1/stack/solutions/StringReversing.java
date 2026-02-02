2a16eee0c86afff0f1525397622fad3ac5b9b005
mazlum emre girgin <mazlumemregirgin@msn.com>
2026-02-02 15:29:40 +0300

    reverse string and balanced paranthesess questions solved.

--- src/main/java/week1/stack/solutions/StringReversing.java
+++ src/main/java/week1/stack/solutions/StringReversing.java
@@ -1,4 +1,39 @@
 package week1.stack.solutions;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class StringReversing {
+    //Açıklama: Kullanıcıdan alınan bir kelimeyi (ör. "Mugla") bir Stack kullanarak ters çeviren kodu yazınız.
+
+    // 1- kullanıcıdan input olarak kelime almalıyım
+    // 2- kelimedeki tüm harfleri bir stacke pushlamalıyım.
+    // 3- stackteki tüm harfleri poplamalıyım
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a Word:");
+        String word = scanner.nextLine();
+
+        if (word.isEmpty()) {
+            System.out.println("Word is Empty");
+        }
+        else {
+            int wordLength = word.length();
+
+            Stack<Character> stack = new Stack<>();
+
+            for (int i =0; i<wordLength; i++) {
+                stack.push(word.charAt(i));
+            }
+
+            while (!stack.isEmpty()) {
+                System.out.print(stack.pop());
+            }
+        }
+
+
+    }
 }
